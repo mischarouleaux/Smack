19355de689c882dd666b2288ef4e119769d28c94
Mischa Rouleaux <mischa-rouleaux@live.nl>
2017-08-21 21:59:31 +0200

    update 12

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -9,15 +9,53 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    //Actions 
+    @IBAction func createAcnntPressed(_ sender: Any) {
+        guard let name = emailTxt.text , emailTxt.text != "" else {return}
+        guard let email = emailTxt.text , emailTxt.text != "" else {return}
+        guard let password = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
+                    if success {
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print("Hello")
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            } 
+                            
+                        })
+                    }
+                })
+            }
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
-
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
 }
--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -12,8 +12,12 @@ typealias CompletionHandler = (_ Succes: Bool) -> ()
 
 //URL Constants
 let BASE_URL = "https://devlopeschatapp.herokuapp.com/v1/"
-
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
+
+//Headers
+let HEADER = ["Content-Type": "application/json; charset=utf-8"]
 
 //Segues
 let TO_LOGIN = "toLogin"
